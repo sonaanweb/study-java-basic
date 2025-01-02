8bbd0d28a13778108fd7df862ec81e770fb4ee6e
Sona <objet_an@naver.com>
2025-01-02 23:11:46 +0900

    feat: OCP

--- src/poly/car1/CarMain1.java
+++ src/poly/car1/CarMain1.java
@@ -14,5 +14,10 @@ public class CarMain1 {
         Model3Car model3Car = new Model3Car();
         driver.setCar(model3Car);   // model3 인스턴스로 변경
         driver.drive();
+
+        // 차량 변경(model3 -> NewCar)
+        NewCar newCar = new NewCar();
+        driver.setCar(newCar);
+        driver.drive();
     }
 }
